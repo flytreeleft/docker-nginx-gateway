c5476350cd8c357a18f340c7daf34fe1826407f5
flytreeleft <flytreeleft@126.com>
2019-01-09 16:59:46 +0800

    Update build.sh for creating the latest tag

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 IMAGE_VERSION=1.11.2-r3
 IMAGE_NAME=flytreeleft/nginx-gateway
 
-docker build --rm -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+docker build --rm -t ${IMAGE_NAME}:${IMAGE_VERSION} . \
+    && docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}
 #docker save ${IMAGE_NAME}:${IMAGE_VERSION} > nginx-gateway.img.tar
-#docker push ${IMAGE_NAME}:${IMAGE_VERSION}
+#docker push ${IMAGE_NAME}:${IMAGE_VERSION} && docker push ${IMAGE_NAME}
 
-docker build --rm --build-arg enable_geoip=true -t ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION} .
+docker build --rm --build-arg enable_geoip=true -t ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION} . \
+    && docker tag ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION} ${IMAGE_NAME}
 #docker save ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION} > nginx-gateway-with-geoip.img.tar
-#docker push ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION}
+#docker push ${IMAGE_NAME}-with-geoip:${IMAGE_VERSION} && docker push ${IMAGE_NAME}-with-geoip
