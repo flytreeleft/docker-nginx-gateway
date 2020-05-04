5665fdcba05c14082228263aff0ff53c6a8d4afb
flytreeleft <flytreeleft@126.com>
2020-05-04 22:40:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,14 +9,14 @@ docker build \
         -f "${DIR}/Dockerfile" \
         "${DIR}" \
     && docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}
-#docker save ${IMAGE_NAME}:${IMAGE_VERSION} > nginx-gateway.img.tar
+docker save ${IMAGE_NAME}:${IMAGE_VERSION} > nginx-gateway.img.tar
 #docker push ${IMAGE_NAME}:${IMAGE_VERSION} && docker push ${IMAGE_NAME}
 
-docker build \
-        --build-arg enable_geoip=true \
-        -t ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} \
-        -f "${DIR}/Dockerfile" \
-        "${DIR}" \
-    && docker tag ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} ${IMAGE_GEOIP_NAME}
+# docker build \
+#         --build-arg enable_geoip=true \
+#         -t ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} \
+#         -f "${DIR}/Dockerfile" \
+#         "${DIR}" \
+#     && docker tag ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} ${IMAGE_GEOIP_NAME}
 #docker save ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} > nginx-gateway-with-geoip.img.tar
 #docker push ${IMAGE_GEOIP_NAME}:${IMAGE_VERSION} && docker push ${IMAGE_GEOIP_NAME}
